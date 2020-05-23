66717d8cd3a9f49c9fd7f0d8c145aa63299795b9
narekacy <gurjiev42@gmail.com>
2020-05-24 00:37:46 +0300

    add 'Dialogs' component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Header from './components/header/header';
 import Navbar from './components/navbar/navbar';
 import Profile from './components/profile/profile';
+import Dialogs from "./components/dialogs/dialogs";
 
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Dialogs/>
+            </div>
+            <Profile />
+        </div>
+    );
 };
 
 export default App;
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Post from './my-posts/my-posts';
-import s from './profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src='https://steamcdn-a.akamaihd.net/steam/apps/585690/header.jpg?t=1490035625' />
       </div>

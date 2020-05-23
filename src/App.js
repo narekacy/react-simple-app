fe61f1fa00c360e03a898298afc913e9237014c3
narekacy <gurjiev42@gmail.com>
2020-05-23 15:54:59 +0300

    add first components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Header from './header'
+import Technologies from './technologies'
 import './App.css';
 
 const App = () => {
@@ -6,29 +8,6 @@ const App = () => {
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div className="App">
-      <ul>
-        <li>Text 1</li>
-        <li>Text 2</li>
-        <li>Text 3</li>
-        <li>Text 4</li>
-      </ul>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div>
-      <a href='#'>Home</a>
-      <a href='#'>News</a>
-      <a href='#'>Page</a>
     </div>
   );
 }
---
+++ src/technologies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+      <div className="App">
+        <ul>
+          <li>Text 1</li>
+          <li>Text 2</li>
+          <li>Text 3</li>
+          <li>Text 4</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Technologies;

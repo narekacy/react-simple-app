f1d27fb58a0213d9a8ef25dd3b8bf74bc9adbcf3
narekacy <gurjiev42@gmail.com>
2020-05-24 14:40:07 +0300

    add 'Dialogs'

---
+++ src/components/dialogs/dialogs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './dialogs.module.css';
+
+const Dialogs = (props) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;

9a839fb756344cf6d7c6989300c3cfb8b771f381
narekacy <gurjiev42@gmail.com>
2020-05-28 01:30:56 +0300

    add 'connect' method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store';
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "./store-context";
+import {Provider} from "react-redux";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(

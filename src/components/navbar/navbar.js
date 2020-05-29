6bf098b828e1ec14eee7e307e13444fc479bfd43
narekacy <gurjiev42@gmail.com>
2020-05-29 18:50:46 +0300

    add 'users'

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,25 +3,28 @@ import s from './navbar.module.css';
 import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <nav className={s.nav}>
-      <div className={s.item}>
-        <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
-      </div>
-      <div className={`${s.item} ${s.active}`}>
-        <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
-      </div>
-      <div className={s.item}>
-        <a>News</a>
-      </div>
-      <div className={s.item}>
-        <a>Music</a>
-      </div>
-      <div className={s.item}>
-        <a>Settings</a>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
+            </div>
+            <div className={`${s.item} ${s.active}`}>
+                <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+            </div>
+            <div className={s.item}>
+                <a>News</a>
+            </div>
+            <div className={s.item}>
+                <a>Music</a>
+            </div>
+            <div className={s.item}>
+                <a>Settings</a>
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,88 +1,52 @@
 import React from "react";
+import defaultAvatar from "../../assets/images/default-avatar.png";
 import s from "./users.module.css";
 import * as axios from "axios";
 
-let Users = (props) => {
-
-    if(props.users.length === 0) {
-        // axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        //     .then(response => {
-        //         debugger;
-        //         props.setUsers();
-        // });
-
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoURL: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2b/2bcb2def5dbdec4116787580334bee07f1191601_full.jpg',
-                    followed: false,
-                    fullName: 'Karen',
-                    status: 'boss',
-                    location: {city: 'Cherkassy', country: 'Ukraine'}
-                },
-                {
-                    id: 2,
-                    photoURL: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2b/2bcb2def5dbdec4116787580334bee07f1191601_full.jpg',
-                    followed: true,
-                    fullName: 'Arsen',
-                    status: 'boss',
-                    location: {city: 'Lutsk', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoURL: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2b/2bcb2def5dbdec4116787580334bee07f1191601_full.jpg',
-                    followed: false,
-                    fullName: 'Artem',
-                    status: 'boss',
-                    location: {city: 'Munchen', country: 'Germany'}
-                },
-                {
-                    id: 4,
-                    photoURL: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2b/2bcb2def5dbdec4116787580334bee07f1191601_full.jpg',
-                    followed: true,
-                    fullName: 'Dania',
-                    status: 'boss',
-                    location: {city: 'Lutsk', country: 'Ukraine'}
-                },
-                {
-                    id: 5,
-                    photoURL: 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2b/2bcb2def5dbdec4116787580334bee07f1191601_full.jpg',
-                    followed: false,
-                    fullName: 'Edgar',
-                    status: 'boss',
-                    location: {city: 'Tbilisi', country: 'Georgia'}
-                }
-            ],
-        );
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        if (this.props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    this.props.setUsers(response.data.items);
+                });
+        }
     }
 
-    return (
-        props.users.map(u =>
-            <div key={u.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u =>
+                            <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoURL} className={s.userPhoto}/>
+                            <img src={u.photos.small !== null ? u.photos.small : defaultAvatar}
+                                 className={s.userPhoto}/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => props.unFollow(u.id)}>Unfollow</button>
-                                : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                                ? <button onClick={() => this.props.unFollow(u.id)}>Unfollow</button>
+                                : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
                         </div>
                     </span>
-                <span>
+                                <span>
                         <span>
                             <div>{u.fullName}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>u.location.country</div>
+                            <div>u.location.city</div>
                         </span>
                     </span>
+                            </div>
+                    )
+                }
             </div>
         )
-    )
-};
+    }
+}
 
 export default Users;

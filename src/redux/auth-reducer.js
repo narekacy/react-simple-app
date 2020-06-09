015c4800101845e95069d459537356e9c9485173
narekacy <gurjiev42@gmail.com>
2020-06-09 19:26:30 +0300

    refactor code

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import {authAPI} from "../api/api";
+
 const SET_USER_DATA = 'SET-USER-DATA';
 
 let initialState = {
@@ -18,6 +20,16 @@ const authReducer = (state = initialState, action) => {
         default:
             return state;
     }
+};
+
+export const getAuthUserData = () => (dispatch) => {
+    authAPI.me()
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                let {id, email, login} = response.data.data;
+                dispatch(setAuthUserData(id, email, login));
+            }
+        });
 };
 
 export const setAuthUserData = (userId, email, login) =>
